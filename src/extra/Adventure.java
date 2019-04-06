489b1ac9d5cf2700967556e9e0188708608b1229
ComputerCodingMaster <league@Mac-mini-4.local>
2019-04-06 11:22:25 -0700

    Im starting off a new project! :D
    lol

---
+++ src/extra/Adventure.java
@@ -0,0 +1,11 @@
+package extra;
+
+import javax.swing.JOptionPane;
+
+public class Adventure {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+JOptionPane.showMessageDialog(" The date is August 24th 2018,the biggest tournament in DoTA 2 is starting tommorow and you are going to participate in it.You're name is Topias Taavitsainen,also known as Topson. You play as the mid-laner for team OG a team who has been slowly losing prestige since last year after losing a row of games.");
+}
