e03abf2d5e1b1b85ee77e862c625f06d98b458ff
ComputerCodingMaster <40555531+ComputerCodingMaster@users.noreply.github.com>
2019-04-13 11:18:32 -0700

    done with adventure
    h

--- src/extra/Adventure.java
+++ src/extra/Adventure.java
@@ -6,6 +6,16 @@ public class Adventure {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		JOptionPane.showMessageDialog(null,
+				"2019,March 31st. You are Pewdiepie and the subscriber gap is 110,000 subs in favor of T-series.");
+String	danke = JOptionPane.showInputDialog(
+				"Do you go to India and eat Tikka Masala,or do you make a music video called Congratulations.");
+if(danke.equalsIgnoreCase("eat tikka masala")){
+	JOptionPane.showMessageDialog(null,"Good job pewds,you have now given up to T-Series and an organization that has multiple human rights abuses has beaten an indivual at a Youtube milestone showing that Youtube as a platform is in trouble.");
+}
+else if(danke.equalsIgnoreCase("make a music video called congratulations")){
+JOptionPane.showMessageDialog(null,"Good job Pewdiepie you have now shown that the Human race as a species still has hope." );	
 	}
-JOptionPane.showMessageDialog(" The date is August 24th 2018,the biggest tournament in DoTA 2 is starting tommorow and you are going to participate in it.You're name is Topias Taavitsainen,also known as Topson. You play as the mid-laner for team OG a team who has been slowly losing prestige since last year after losing a row of games.");
+}
 }
